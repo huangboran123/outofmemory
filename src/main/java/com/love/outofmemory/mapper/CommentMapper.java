6e61516962a05f3ff473d3eb868a02af63e4a5b7
huang <1813422657@qq.com>
2021-02-18 13:31:24 +0800

    评论提交回复2

--- src/main/java/com/love/outofmemory/mapper/CommentMapper.java
+++ src/main/java/com/love/outofmemory/mapper/CommentMapper.java
@@ -17,12 +17,14 @@ public interface CommentMapper {
     @Insert("insert into o_comment values(null,#{critic.id},#{comment_time},#{comment_content},#{blog.id})")
     int commitcomment(Comment comment1);
 
-    @Select("select o_comment.id,critic_id,comment_time,comment_content,image from o_comment inner join o_user on o_comment.critic_id=o_user.id   where blog_id=#{blogId} order by comment_time desc limit #{start},#{counts} ")
+    @Select("select o_comment.id,critic_id,comment_time,comment_content,image,username from o_comment inner join o_user on o_comment.critic_id=o_user.id   where blog_id=#{blogId} order by comment_time desc limit #{start},#{counts} ")
     @Results(id="comment",value = {
 
             @Result(column = "critic_id",property = "critic.id"),
             @Result(column = "o_comment.id",property = "id"),
-            @Result(column = "image",property = "critic.image")
+            @Result(column = "image",property = "critic.image"),
+            @Result(column = "username",property = "critic.username")
+
 
     })
     List<Comment> getAllCommentsPageByBlogId(Integer blogId,Integer start, Integer counts);
